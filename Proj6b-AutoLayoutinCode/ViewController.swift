7feae4129c618fa187dd9fe5c3dc81d942625a66
Sain-R Edwards <ske@SKE-iMac.local>
2018-06-25 04:07:56 -0700

    1st commit

---
+++ Proj6b-AutoLayoutinCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proj6b-AutoLayoutinCode
+//
+//  Created by Sain-R Edwards on 6/25/18.
+//  Copyright © 2018 Swift Koding 4 Everyone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
